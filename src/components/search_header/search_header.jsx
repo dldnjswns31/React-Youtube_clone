959cba89ff3b6d705efb0a36b2a0eb4d5c36b76a
dldnjswns31 <dldnjswns32@gmail.com>
2022-01-20 15:55:34 +0900

    add youtube service

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,15 +1,34 @@
 import styles from "./search_header.module.css";
-import React from "react";
+import React, { useRef } from "react";
 
-const SearchHeader = (props) => {
+const SearchHeader = ({ onSearch }) => {
+  const inputRef = useRef();
+  const handleSearch = () => {
+    const value = inputRef.current.value;
+    onSearch(value);
+  };
+  const onClick = () => {
+    handleSearch();
+  };
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
         <img className={styles.image} src="/images/logo.png" alt="logo" />
         <h1 className={styles.title}>Youtube</h1>
       </div>
-      <input className={styles.input} type="search" placeholder="Search..." />
-      <button className={styles.button} type="submit">
+      <input
+        ref={inputRef}
+        className={styles.input}
+        type="search"
+        placeholder="Search..."
+        onKeyPress={onKeyPress}
+      />
+      <button className={styles.button} type="submit" onClick={onClick}>
         <img
           className={styles.buttonImg}
           src="/images/search.png"
